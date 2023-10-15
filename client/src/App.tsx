783cb7b1df06a58b2b60d644c19195f5bae87394
vamsi422 <vamshikrishna7942@gmail.com>
2023-10-15 14:13:35 +0530

    created pages(Home,About,SingnIn,SignOut,Profile) and added routes to those pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
+import About from './pages/About'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>        
+        <Route path='/profile' element={<SignOut/>}/>        
+        <Route path='/signin' element={<SignIn/>}/>
+        <Route path='/signout' element={<Profile/>}/>        
+        
+        
+      </Routes>
+    </BrowserRouter>
   )
 }
 
